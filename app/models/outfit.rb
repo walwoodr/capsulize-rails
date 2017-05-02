9cbf6ab2e582da2d9e01d6bd92f13296b31c5e5c
walwoodr <walwoodr@gmail.com>
2017-05-02 18:49:03 -0500

    Add validation of user to outfit

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -2,4 +2,6 @@ class Outfit < ApplicationRecord
   has_many :outfit_clothing_items
   has_many :clothing_items, through: :outfit_clothing_items
   belongs_to :user
+
+  validates :user, presence: true
 end
