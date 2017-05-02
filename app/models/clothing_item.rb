66b28246ae20e4716276eb117fd2134b36bc3043
walwoodr <walwoodr@gmail.com>
2017-05-02 16:42:40 -0500

    Add category association and validation to clothing item

--- app/models/clothing_item.rb
+++ app/models/clothing_item.rb
@@ -3,5 +3,8 @@ class ClothingItem < ApplicationRecord
   validates :color, presence: true
   validates :fanciness, presence: true,
                         inclusion: { in: 1..5 }
+  validates :category, presence: true
+
+  belongs_to :category
 
 end
