bcb23fc68478ee61402f1f1a92983091d6711683
walwoodr <walwoodr@gmail.com>
2017-05-02 16:11:02 -0500

    Add validations to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ApplicationRecord
+  validates :part_of_body, presence: true,
+                           inclusion: { in: %w(legs arms torso wrist finger hand(s) body head neck waist feet ankles ears shoulders), message: "%{value} is not a valid body part." }
+  validates :name, presence: true
+
+
+
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   let(:attributes) do
-    {name: "Pants"}
+    {name: "Pants", part_of_body: "legs"}
   end
 
   it 'is valid with a name and body part' do
@@ -18,25 +18,25 @@ RSpec.describe Category, type: :model do
   end
 
   it 'is valid with a valid body part' do
-    expect(Category.new(name: "legs", name: "Pants")).to be_valid
-    expect(Category.new(name: "torso", name: "Shirt")).to be_valid
-    expect(Category.new(name: "wrist", name: "Bracelet")).to be_valid
+    expect(Category.new(part_of_body: "legs", name: "Pants")).to be_valid
+    expect(Category.new(part_of_body: "torso", name: "Shirt")).to be_valid
+    expect(Category.new(part_of_body: "wrist", name: "Bracelet")).to be_valid
   end
 
   it 'is invalid with an invalid body part' do
-    expect(Category.new(name: "piano", name: "Pants")).not_to be_valid
-    expect(Category.new(name: "elephant", name: "Shirt")).not_to be_valid
+    expect(Category.new(part_of_body: "piano", name: "Pants")).not_to be_valid
+    expect(Category.new(part_of_body: "elephant", name: "Shirt")).not_to be_valid
   end
 
   it 'has many clothing items' do
-    expect(Category.first.clothing_items.size).not_to be(nil)
+    expect(Category.new(attributes).clothing_items.size).not_to be(nil)
   end
 
   it 'has a name' do
-    expect(Category.first.name).not_to be(nil)
+    expect(Category.new(attributes).name).to eq(attributes[:name])
   end
 
   it 'has a part of the body' do
-    expect(Category.first.part_of_body).not_to be(nil)
+    expect(Category.new(attributes).part_of_body).to eq(attributes[:part_of_body])
   end
 end
