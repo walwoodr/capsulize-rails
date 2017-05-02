f0bdcc0f3e3bee9eed2bcf9fb8910f8a817bc7ee
walwoodr <walwoodr@gmail.com>
2017-05-02 16:18:14 -0500

    Add specs for clothing item validation

--- spec/models/clothing_item_spec.rb
+++ spec/models/clothing_item_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe ClothingItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_item) do
+    ClothingItem.new(name: "Jeans", color: "blue", fanciness: 2)
+  end
+
+  describe 'validations' do
+    it 'is valid with a name, color and fanciness' do
+      expect(valid_item).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      no_name = Clothing_Item.new(color: "blue", fanciness: 2)
+
+      expect(no_name).not_to be_valid
+    end
+
+    it 'is not valid without a color' do
+      no_color = Clothing_Item.new(name: "Jeans", fanciness: 2)
+
+      expect(no_color).not_to be_valid
+    end
+
+    it 'defaults to fanciness of 3' do
+      unset_fanciness = Clothing_Item.new(name: "Jeans", color: "blue")
+
+      expect(unset_fanciness).to be_valid
+      expect(unset_fanciness.fanciness).to eq(3)
+    end
+  end
+
+
 end
