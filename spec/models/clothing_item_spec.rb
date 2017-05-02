8dde111ff8c20485fee64f524865461b740e9c30
walwoodr <walwoodr@gmail.com>
2017-05-02 16:41:39 -0500

    Add specs for clothing item / category association

--- spec/models/clothing_item_spec.rb
+++ spec/models/clothing_item_spec.rb
@@ -1,52 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe ClothingItem, type: :model do
-  let(:valid_item) do
-    ClothingItem.new(name: "Jeans", color: "blue", fanciness: 2)
-  end
+  let(:pants) { Category.new(name: "pants", part_of_body: "legs") }
+  let(:valid_item) { ClothingItem.new(name: "Jeans", color: "blue", fanciness: 2, category: pants) }
 
   describe 'validates' do
-    it 'is valid with a name, color and fanciness' do
+    it 'is valid with a name, color, category and fanciness' do
       expect(valid_item).to be_valid
     end
 
     it 'is not valid without a name' do
-      no_name = ClothingItem.new(color: "blue", fanciness: 2)
+      no_name = ClothingItem.new(color: "blue", fanciness: 2, category: pants)
 
       expect(no_name).not_to be_valid
     end
 
     it 'is not valid without a color' do
-      no_color = ClothingItem.new(name: "Jeans", fanciness: 2)
+      no_color = ClothingItem.new(name: "Jeans", fanciness: 2, category: pants)
 
       expect(no_color).not_to be_valid
     end
 
-    it 'defaults to fanciness of 3' do
-      unset_fanciness = ClothingItem.new(name: "Jeans", color: "blue")
+    it 'is not valid without a category' do
+      no_category = ClothingItem.new(name: "Jeans", fanciness: 2, color: "blue")
 
-      expect(unset_fanciness).to be_valid
-      expect(unset_fanciness.fanciness).to eq(3)
+      expect(no_category).not_to be_valid
     end
 
-    it 'fanciness is valid between 1 and 5' do
-      fanciness_one = ClothingItem.new(name: "Jeans", color: "blue", fanciness: 1)
-      fanciness_three = ClothingItem.new(name: "Jeans", color: "blue", fanciness: 3)
-      fanciness_five = ClothingItem.new(name: "Jeans", color: "blue", fanciness: 5)
+    describe 'fanciness' do
+      it 'defaults to 3' do
+        unset_fanciness = ClothingItem.new(name: "Jeans", color: "blue", category: pants)
 
-      expect(fanciness_one).to be_valid
-      expect(fanciness_three).to be_valid
-      expect(fanciness_five).to be_valid
-    end
+        expect(unset_fanciness).to be_valid
+        expect(unset_fanciness.fanciness).to eq(3)
+      end
 
-    it 'fanciness is invalid between 1 and 5' do
-      fanciness_zero = ClothingItem.new(name: "Jeans", color: "blue", fanciness: 0)
-      fanciness_twelve = ClothingItem.new(name: "Jeans", color: "blue", fanciness: 12)
+      it 'is valid between 1 and 5' do
+        fanciness_one = ClothingItem.new(name: "Jeans", color: "blue", fanciness: 1, category: pants)
+        fanciness_three = ClothingItem.new(name: "Jeans", color: "blue", fanciness: 3, category: pants)
+        fanciness_five = ClothingItem.new(name: "Jeans", color: "blue", fanciness: 5, category: pants)
 
-      expect(fanciness_zero).not_to be_valid
-      expect(fanciness_twelve).not_to be_valid
+        expect(fanciness_one).to be_valid
+        expect(fanciness_three).to be_valid
+        expect(fanciness_five).to be_valid
+      end
+
+      it 'is invalid between 1 and 5' do
+        fanciness_zero = ClothingItem.new(name: "Jeans", color: "blue", fanciness: 0, category: pants)
+        fanciness_twelve = ClothingItem.new(name: "Jeans", color: "blue", fanciness: 12, category: pants)
+
+        expect(fanciness_zero).not_to be_valid
+        expect(fanciness_twelve).not_to be_valid
+      end
     end
   end
 
+  describe 'associations' do
+
+    it 'belongs to a category' do
+      expect(valid_item.category).to eq(pants)
+    end
+
+  end
 
 end
