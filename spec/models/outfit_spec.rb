8c27c58a54637b2b88b09763b023dc9c0eeb251a
walwoodr <walwoodr@gmail.com>
2017-05-02 18:17:09 -0500

    Specs for outfit associations

--- spec/models/outfit_spec.rb
+++ spec/models/outfit_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Outfit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    it 'is valid with a name and two or more clothing items' do
+      outfit = Outfit.new(name: "Fave outfit", clothing_item_ids: [3, 4])
+
+      expect(outfit).to be_valid
+    end
+
+    it 'is valid with items that belong to the user' do
+      user = User.first
+      outfit = user.outfits.build(clothing_items: [user.clothing_items.first, user.clothing_items.last])
+
+      expect(outfit).to be_valid
+    end
+  end
+
+  describe 'associations' do
+    # TODO add specs
+  end
+
+  describe 'class methods' do
+    # TODO add specs
+    xit 'adds clothing items to the user if the user doesn\'t already own the item' do
+      #write a spec for this
+    end
+  end
+
 end
