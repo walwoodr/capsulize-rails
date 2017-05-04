e0387fb6e1508f5c9c333811dc12a35005a610b2
walwoodr <walwoodr@gmail.com>
2017-05-04 11:43:51 -0500

    Add routes and views for Clothing Items Controller

--- app/controllers/clothing_items_controller.rb
+++ app/controllers/clothing_items_controller.rb
@@ -1,2 +1,35 @@
 class ClothingItemsController < ApplicationController
+
+    before_action :find_item, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!
+
+    def index
+      @clothing_items = ClothingItem.all
+    end
+
+    def show
+    end
+
+    def new
+      @clothing_item = ClothingItem.new
+    end
+
+    def create
+      @clothing_item = ClothingItem.new
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def find_item
+      @clothing_item = ClothingItem.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
+  root to: 'outfits#index'
+  
   devise_for :users
-  resources :categories
+  resources :categories, only: [:index]
   resources :clothing_items
   resources :users do
     resources :outfits
   end
-  root to: 'outfits#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
