021232460c9a36adcca5959d1f34d9900a331d6d
walwoodr <walwoodr@gmail.com>
2017-05-04 14:01:58 -0500

    Add specs for outfit index page

--- spec/features/outfit_feature_spec.rb
+++ spec/features/outfit_feature_spec.rb
@@ -1,25 +1,48 @@
-describe 'user logged in', type: :feature do
+require 'rails_helper'
 
-  describe 'outfit index', type: :feature do
-    it 'has a number of outfit divs matching number of outfits' do
+RSpec.describe 'user logged in', type: :feature do
+  let(:category) {Category.first}
+  let(:user) {User.first}
+  let(:outfit) { user.outfits.first }
+  let(:clothing_item) { outfit.clothing_items.first }
 
+  before do
+    login_as(user)
+  end
+
+  describe 'outfits index page' do
+    before do
+      visit user_outfits_path(user)
     end
 
-    it 'renders an outfit partial' do
+    it 'has a number of outfit divs matching number of outfits' do
+      expect(page).to have_css('div.outfit', count: user.outfits.size)
+    end
 
+    it 'displays information about the outfit' do
+      expect(page).to have_text(user.outfits.first.name)
+      expect(page).to have_text(user.outfits.second.clothing_items.first.name)
+      expect(page).to have_text(user.outfits.last.clothing_items.first.color)
     end
 
     it 'displays a link to create a new outfit' do
-
+      expect(page).to have_link("New Outfit")
+      expect(page).to have_css('a#new-outfit')
+      expect(page).to have_xpath(:href => user_new_outfit_path)
     end
   end
 
-  describe 'outfit show', type: :feature do
-    it 'displays the outfit\'s name' do
+  describe 'outfit show' do
+    before do
+      visit user_outfit_path(user, outfit)
+    end
 
+    it 'displays the outfit\'s name' do
+      expect(page).to have_text(outfit.name)
     end
 
     it 'displays a link to edit the outfit' do
+      expect(page).to have_text(outfit.name)
 
     end
 
@@ -28,7 +51,7 @@ describe 'user logged in', type: :feature do
     end
   end
 
-  describe 'new outfit page', type: :feature do
+  describe 'new outfit page' do
     it 'displays a form to create a new outfit' do
 
     end
@@ -42,7 +65,7 @@ describe 'user logged in', type: :feature do
     end
   end
 
-  describe 'new outfit creation', type: :feature do
+  describe 'new outfit creation' do
     it 'creates a new outfit belonging to the user' do
 
     end
@@ -52,7 +75,7 @@ describe 'user logged in', type: :feature do
     end
   end
 
-  describe 'outfit modification page', type: :feature do
+  describe 'outfit modification page' do
     it 'displays a form to change the existing outfit' do
 
     end
@@ -70,7 +93,7 @@ describe 'user logged in', type: :feature do
     end
   end
 
-  describe 'modify outfit', type: :feature do
+  describe 'modify outfit' do
     it 'changes the outfit belonging to the user' do
       # does not create a new outfit
     end
@@ -80,7 +103,7 @@ describe 'user logged in', type: :feature do
     end
   end
 
-  describe 'outfit deletion', type: :feature do
+  describe 'outfit deletion' do
     it 'deletes the outfit' do
 
     end
