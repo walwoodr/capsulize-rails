b3c94e36a8a7b2f2ee34740ee91886d84a0c6c86
walwoodr <walwoodr@gmail.com>
2017-05-04 10:17:28 -0500

    Adjustments to factories

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,6 +1,22 @@
 FactoryGirl.define do
-  factory :category do
+  factory :pants, class: Category do
     name "Pants"
     part_of_body "legs"
+  end
+  factory :shirts, class: Category do
+    name "Shirts"
+    part_of_body "torso"
+  end
+  factory :layers, class: Category do
+    name "Layers"
+    part_of_body "torso"
+  end
+  factory :shoes, class: Category do
+    name "Shoes"
+    part_of_body "feet"
+  end
+  factory :jewelry, class: Category do
+    name "Jewelry"
+    part_of_body "torso"
   end
 end
--- spec/factories/outfits.rb
+++ spec/factories/outfits.rb
@@ -1,12 +1,10 @@
 FactoryGirl.define do
   factory :outfit do
-    user :user
-    clothing_items [:jeans, :tshirt]
+    
   end
 
   factory :outfit_2, class: Outfit do
-    user :user_2
-    clothing_items [:jeans, :necklace, :tshirt, :vest]
+
   end
 
 end
