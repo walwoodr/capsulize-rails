fa8a55247e9a7aa5cc06670fc8c6a59419156ad0
walwoodr <walwoodr@gmail.com>
2017-05-04 09:08:26 -0500

    Add factories for outfits and clothing items

--- spec/factories/clothing_items.rb
+++ spec/factories/clothing_items.rb
@@ -1,6 +1,23 @@
 FactoryGirl.define do
-  factory :clothing_item do
+  factory :jeans, class: ClothingItem do
     name "Jeans"
     color "blue"
+  end
+  factory :tshirt, class: ClothingItem do
+    name "T-shirt"
+    color "blue"
+  end
+  factory :sneakers, class: ClothingItem do
+    name "Sneakers"
+    color "blue"
+  end
+  factory :necklace, class: ClothingItem do
+    name "Necklace"
+    color "blue"
+  end
+  factory :vest, class: ClothingItem do
+    name "Vest"
+    color "blue"
+    fanciness 4
   end
 end
