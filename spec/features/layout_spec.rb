79fd462dc97713249044073d85e511ef6a3363b0
walwoodr <walwoodr@gmail.com>
2017-05-04 12:15:43 -0500

    Outline of layout features spec

---
+++ spec/features/layout_spec.rb
@@ -0,0 +1,25 @@
+describe 'user logged in', type: :feature do
+  it 'displays authentication navigation' do
+    # sign out link
+    # modify account link
+  end
+
+  it 'displays app navigation' do
+    # home link
+    # outfits link
+    # categories link
+    # clothing items link
+    # EVENTUALLY - what to buy link
+    # EVENTUALLY - friends link
+    # EVENTUALLY - explore other's outfits link
+  end
+end
+
+describe 'user not logged in' do
+
+  it 'displays logged out navigation' do
+    # home
+    # sign up link
+    # sign in link
+  end
+end
