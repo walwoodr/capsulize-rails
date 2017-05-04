69dfd7f9054997b291a682ca2e44c58a766b8a1b
walwoodr <walwoodr@gmail.com>
2017-05-04 09:33:20 -0500

    Add guard, spring, capybara testing gems

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,4 +55,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # config for warden, factorygirl and devise
+  config.include FactoryGirl::Syntax::Methods
+  # config.include Devise::TestHelpers, type: :controller
+  # config.include Devise::Test::ControllerHelpers
+  config.include Warden::Test::Helpers
 end
