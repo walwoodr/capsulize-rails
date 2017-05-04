542a6ccf20d91b44aa9d9391bb42bf855c4da1a7
walwoodr <walwoodr@gmail.com>
2017-05-04 14:51:02 -0500

    Build out outfit index page

--- app/helpers/clothing_items_helper.rb
+++ app/helpers/clothing_items_helper.rb
@@ -1,2 +1,23 @@
 module ClothingItemsHelper
+
+  def word_fanciness(clothing_item)
+    case clothing_item.fanciness
+    when 1
+      "scruffy"
+    when 2
+      "comfy"
+    when 3
+      "nice"
+    when 4
+      "spiffy"
+    when 5
+      "fancy"
+    end
+  end
+  # scruffy
+  # comfy
+  # nice
+  # spiffy
+  # fancy
+
 end
--- spec/helpers/clothing_items_helper_spec.rb
+++ spec/helpers/clothing_items_helper_spec.rb
@@ -11,5 +11,11 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe ClothingItemsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  xit 'renders fanciness level as words' do
+    # scruffy
+    # comfy
+    # nice
+    # spiffy
+    # fancy
+  end
 end
