aaba8d061aef474906792eeae8d6b4670f5799bc
walwoodr <walwoodr@gmail.com>
2017-05-04 14:20:48 -0500

    Add specs for new outfit page and creation

--- spec/features/outfit_feature_spec.rb
+++ spec/features/outfit_feature_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe 'user logged in', type: :feature do
     it 'displays a link to create a new outfit' do
       expect(page).to have_link("New Outfit")
       expect(page).to have_css('a#new-outfit')
-      expect(page).to have_xpath(:href => user_new_outfit_path)
+      expect(page).to have_xpath(:href => new_user_outfit_path)
     end
   end
 
@@ -42,36 +42,54 @@ RSpec.describe 'user logged in', type: :feature do
     end
 
     it 'displays a link to edit the outfit' do
-      expect(page).to have_text(outfit.name)
-
+      expect(page).to have_link("Edit outfit")
+      expect(page).to have_xpath(:href => edit_user_outfit_path(user, outfit))
     end
 
     xit 'displays a link to riff on the outfit (duplicate & modify)' do
-
+      expect(page).to have_link("Riff off the outfit")
+      expect(page).to have_xpath(:href => new_by_duplicate_user_outfit_path(user, outfit))
     end
   end
 
   describe 'new outfit page' do
-    it 'displays a form to create a new outfit' do
+    before do
+      visit new_user_outfit_path(user)
+    end
 
+    it 'displays a form to create a new outfit' do
+      expect(page).to have_css('form#outfit')
     end
 
     it 'displays clothes options by category' do
-
+      expect(page).to have_text(category.name)
+      expect(page).to have_css("check_box.#{category.name}.clothing_item", count: category.clothing_items.size)
     end
 
-    it 'allows you to create a new item of clothing on the outfit' do
-
+    it 'allows you to create a new item of clothing with the outfit' do
+      expect(page).to have_css('input.clothing_item[name]')
+      expect(page).to have_css('input.clothing_item[color]')
     end
   end
 
   describe 'new outfit creation' do
-    it 'creates a new outfit belonging to the user' do
+    before do
+      visit new_user_outfit_path(user)
+      fill_in "Name", with: "Evening at the Opera"
+      # select "black Trousers", from "Pants"
+      # select "White Blouse", from "Shirts"
+      click_button "Create Outfit"
+    end
 
+    let (:outfit_2) { Outfit.find_by(name: "Evening at the Opera")}
+
+    it 'creates a new outfit belonging to the user' do
+      expect(outfit_2).not_to be_nil
+      expect(user.outfits.last).to eq(outfit_2)
     end
 
     it 'redirects to view the new outfit' do
-
+      expect(page).to have_text(outfit_2.name)
     end
   end
 
