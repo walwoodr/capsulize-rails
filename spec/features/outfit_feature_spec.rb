fe912eea636f79ebe4a66b85325053ddda982ea4
walwoodr <walwoodr@gmail.com>
2017-05-04 12:15:31 -0500

    Outline of outfit features spec

---
+++ spec/features/outfit_feature_spec.rb
@@ -0,0 +1,100 @@
+describe 'user logged in', type: :feature do
+
+  describe 'outfit index', type: :feature do
+    it 'has a number of outfit divs matching number of outfits' do
+
+    end
+
+    it 'renders an outfit partial' do
+
+    end
+
+    it 'displays a link to create a new outfit' do
+
+    end
+  end
+
+  describe 'outfit show', type: :feature do
+    it 'displays the outfit\'s name' do
+
+    end
+
+    it 'displays a link to edit the outfit' do
+
+    end
+
+    xit 'displays a link to riff on the outfit (duplicate & modify)' do
+
+    end
+  end
+
+  describe 'new outfit page', type: :feature do
+    it 'displays a form to create a new outfit' do
+
+    end
+
+    it 'displays clothes options by category' do
+
+    end
+
+    it 'allows you to create a new item of clothing on the outfit' do
+
+    end
+  end
+
+  describe 'new outfit creation', type: :feature do
+    it 'creates a new outfit belonging to the user' do
+
+    end
+
+    it 'redirects to view the new outfit' do
+
+    end
+  end
+
+  describe 'outfit modification page', type: :feature do
+    it 'displays a form to change the existing outfit' do
+
+    end
+
+    it 'displays a button to delete the outfit' do
+
+    end
+
+    it 'displays clothes options by category' do
+
+    end
+
+    it 'allows you to create a new item of clothing on the outfit' do
+
+    end
+  end
+
+  describe 'modify outfit', type: :feature do
+    it 'changes the outfit belonging to the user' do
+      # does not create a new outfit
+    end
+
+    it 'redirects to view the show outfit page' do
+
+    end
+  end
+
+  describe 'outfit deletion', type: :feature do
+    it 'deletes the outfit' do
+
+    end
+
+    it 'redirects to the outfit index page' do
+
+    end
+  end
+
+end
+
+describe 'user not logged in' do
+
+  it 'renders an error message' do
+
+  end
+end
