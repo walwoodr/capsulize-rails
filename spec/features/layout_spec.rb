c1969480f5a5df53ecb25260eaafaa5884bd08b0
walwoodr <walwoodr@gmail.com>
2017-05-04 12:23:25 -0500

    Specs for navigation

--- spec/features/layout_spec.rb
+++ spec/features/layout_spec.rb
@@ -1,25 +1,39 @@
-describe 'user logged in', type: :feature do
-  it 'displays authentication navigation' do
-    # sign out link
-    # modify account link
+require 'rails_helper'
+
+RSpec.feature "Layout", type: :feature do
+  let (:user) { FactoryGirl.create(:user) }
+
+  describe 'user logged in' do
+    before do
+      login_as(user)
+      visit root_path
+    end
+
+    it 'displays authentication navigation' do
+      expect(page).to have_text("Sign Out")
+      expect(page).to have_text("Settings")
+    end
+
+    it 'displays app navigation' do
+      expect(page).to have_text("Home")
+      expect(page).to have_text("Outfits")
+      expect(page).to have_text("Categories")
+      expect(page).to have_text("Clothes")
+      # EVENTUALLY - what to buy link
+      # EVENTUALLY - friends link
+      # EVENTUALLY - explore other's outfits link
+    end
   end
 
-  it 'displays app navigation' do
-    # home link
-    # outfits link
-    # categories link
-    # clothing items link
-    # EVENTUALLY - what to buy link
-    # EVENTUALLY - friends link
-    # EVENTUALLY - explore other's outfits link
-  end
-end
+  describe 'user not logged in' do
 
-describe 'user not logged in' do
-
-  it 'displays logged out navigation' do
-    # home
-    # sign up link
-    # sign in link
+    it 'displays logged out navigation' do
+      expect(page).to have_text("Log In")
+      expect(page).to have_text("Sign Up")
+      # home
+      # sign up link
+      # sign in link
+    end
   end
+
 end
