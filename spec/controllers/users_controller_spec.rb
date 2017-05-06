cde0b3d22ddf50b62d674694cc2b5f48c38d68d6
walwoodr <walwoodr@gmail.com>
2017-05-05 21:40:18 -0500

    Add specs for biggest closet user page action and feature

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  let (:user) { FactoryGirl.create(:user) }
+
+  render_views
+
+  context 'logged in user' do
+
+    before do
+      login_with(user)
+    end
+
+    it 'biggest closet action renders biggest closet template' do
+      get :biggest_closet
+
+      is_expected.to render_template :'users/biggest_closet'
+    end
+
+  end
+
 end
---
+++ spec/features/user_feature_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "Users", type: :feature do
+  let (:user) { FactoryGirl.create(:user) }
+
+  describe 'user logged in' do
+    before do
+      login_as(user)
+    end
+
+    it 'displays biggest closet' do
+      visit user_biggest_closet_path
+      bc_user = User.biggest_closet
+
+      expect(page).to have_text(bc_user.name)
+      expect(page).to have_text("Who has an astonishing #{bc_user.clothing_items.size} items in their closet.")
+    end
+  end
+end
