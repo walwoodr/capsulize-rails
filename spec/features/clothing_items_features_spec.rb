77c7541705ddcabf2100325091e2f7a8d0b0f2d9
walwoodr <walwoodr@gmail.com>
2017-05-08 08:51:19 -0500

    Create specs for clothing items

--- spec/features/clothing_items_features_spec.rb
+++ spec/features/clothing_items_features_spec.rb
@@ -2,37 +2,56 @@ require 'rails_helper'
 
 RSpec.feature "Clothing Items", type: :feature do
   let (:user) { FactoryGirl.create(:user) }
+  let (:pants) { FactoryGirl.create(:pants) }
+  let (:jeans) { FactoryGirl.create(:jeans, category: pants) }
 
   describe 'user logged in' do
     before do
       login_as(user)
+      user.user_clothing_items.find_or_create_by(clothing_item_id: jeans.id)
     end
 
     describe 'index page' do
       before do
-
+        visit clothing_items_path
       end
 
       it 'displays a list of clothing items by category' do
-
+        expect(page).to have_text("pants")
+        expect(page).to have_text("#{jeans.name} | #{jeans.color} | #{word_fanciness(jeans)}")
       end
 
       it 'allows a user to remove a clothing item from their closet' do
+        click_link("remove")
 
+        expect(page).to have_text("#{jeans.color} #{jeans.name} have been removed from your closet")
+        expect(page).not_to have_text("#{jeans.name} | #{jeans.color} | #{word_fanciness(jeans)}")
       end
 
       it 'displays a link for a user to add a new item' do
-
+        expect(page).to have_link("Create new item")
       end
     end
 
-    describe 'add/create item' do
-      it 'displays fields to add item' do
-
+    describe 'new outfit creation' do
+      before do
+        visit clothing_items_path
+        click_link("Create new item")
+        fill_in "Clothing Item Name", with: "Leggings"
+        fill_in "Color", with: "silver"
+        fill_in "fanciness", with: "4"
+        select "pants", from: "Categories"
+        click_button "Create Clothing Item"
       end
 
       it 'adds a new item in the user\'s closet' do
+        leggings = user.clothing_items.last
 
+        expect(leggings).not_to be_nil
+        expect(leggings.name).to eq("Leggings")
+        expect(leggings.color).to eq("silver")
+        expect(leggings.fanciness).to eq(4)
+        expect(page).to have_text("Leggings | silver")
       end
     end
   end
@@ -44,13 +63,16 @@ RSpec.feature "Clothing Items", type: :feature do
   end
 
   describe 'user not logged in' do
-    it 'index redirects to login screen' do
+    it 'index redirect renders an error message' do
+      visit clothing_items_path
 
+      expect(page).to have_text("You need to sign in or sign up before continuing.")
     end
 
-    it 'add redirects to login screen' do
+    it 'add redirect renders an error message' do
+      visit new_clothing_item_path
 
+      expect(page).to have_text("You need to sign in or sign up before continuing.")
     end
   end
-
 end
