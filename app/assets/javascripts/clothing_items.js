4cddbd859b58b437247163a1740bbf78e747b2e2
walwoodr <walwoodr@gmail.com>
2017-05-18 11:57:04 -0500

    Add js helpers to provide clothing item view data

--- app/assets/javascripts/clothing_items.js
+++ app/assets/javascripts/clothing_items.js
@@ -3,3 +3,29 @@ function ClothingItem(json) {
   this.color = json["color"];
   this.fanciness = json["fanciness"];
 }
+
+// Note: this function replicates the word_fanciness helper in clothing_item_helper
+ClothingItem.prototype.wordFanciness = function(){
+  switch(this.fanciness) {
+    case 1:
+      return "scruffy";
+      break;
+    case 2:
+      return "comfy";
+      break;
+    case 3:
+      return "nice";
+      break;
+    case 4:
+      return "spiffy";
+      break;
+    case 5:
+      return "fancy";
+      break;
+  };
+}
+
+// Note: this function replicates the attributes_display helper in clothing_item_helper
+ClothingItem.prototype.attributesDisplay = function(){
+  return `${this.name} | ${this.color} | ${this.wordFanciness()}`
+}
--- app/helpers/clothing_items_helper.rb
+++ app/helpers/clothing_items_helper.rb
@@ -1,5 +1,6 @@
 module ClothingItemsHelper
 
+  # // Note: this function is replicated by the wordFanciness function in clothing_item.js
   def word_fanciness(clothing_item)
     case clothing_item.fanciness
     when 1
@@ -15,6 +16,7 @@ module ClothingItemsHelper
     end
   end
 
+  # // Note: this function is replicated by the attributesDisplay function in clothing_item.js
   def attributes_display(clothing_item)
     "#{clothing_item.name} | #{clothing_item.color} | #{word_fanciness(clothing_item)}"
   end
